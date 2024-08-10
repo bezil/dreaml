43a6f0598b96756e420b203a485bbe28bc60f114
bezil <bezilonnet@gmail.com>
2024-08-10 09:37:12 +0000

    avoid xss attack

--- lib/page.ml
+++ lib/page.ml
@@ -1,2 +1,4 @@
+open Dream
+
 let render param =
-  Printf.sprintf "<html><body><h1>The URL parameter was %s!</h1></body></html>" param
+  Printf.sprintf "<html><body><h1>The URL parameter was %s!</h1></body></html>" (Dream.html_escape param)
